e9a16b93d3e443daf79f902495799566565b306d
anna dodo <anna.dorenskaya@gmail.com>
2021-03-04 22:31:56 +0100

    added the components

--- src/App.js
+++ src/App.js
@@ -1,20 +1,34 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from "react-router-dom";
+import { isMobile, isDesktop } from "react-device-detect";
+
+import Header from "./components/header/Header";
+import Intro from "./components/intro/Intro"
+import About from "./components/about/About"
+import Skills from "./components/skills/Skills"
+import Experience from "./components/experience/Experience"
+import Projects from "./components/projects/Projects"
+import Contacts from "./components/contacts/Contacts"
+import Footer from "./components/footer/Footer"
 
 function App() {
   return (
     <div>
-      <nav></nav>
-      <header></header>
-
-      <section>
-        <article>
-          <h2></h2>
-          <p></p>
-        </article>
-      </section>
-      <aside></aside>
-      <footer></footer>
+      <Router>
+        <Header />
+        <Intro />
+        <About />
+        <Skills />
+        <Experience />
+        <Projects />
+        <Contacts />
+        <Footer />
+      </Router>
     </div>
   );
 }
