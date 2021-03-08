0a4c7d4e502653efb3855e97c530c170f7239a21
anna dodo <anna.dorenskaya@gmail.com>
2021-03-08 22:49:24 +0100

    about, work experience, text only

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
 
 export default function About() {
-    return (
-        <div>
-            About
-        </div>
-    )
+  return (
+    <div>
+      <h4>Hello</h4>
+      <h5>My name is Anna Dorenskaia and I am a full stack webdeveloper</h5>
+      <p>
+        I have a vast experience in testing and a huge desire to switch to the
+        web developer role totally. I broke many apps, now it is time to develop
+        them myself! My best personal quality is that I can go through walls to
+        get to the knowledge that is needed to perform well at whatever task I
+        am working on.
+      </p>
+    </div>
+  );
 }
