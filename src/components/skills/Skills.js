8f3bf1bb1bbd7dc99b7f736efc39d9f5ad5a1d54
anna dodo <anna.dorenskaya@gmail.com>
2021-03-09 00:10:15 +0100

    skills text only

--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -3,7 +3,30 @@ import React from 'react'
 export default function Skills() {
     return (
         <div>
-            Skills
+            <h2>Skills</h2>
+            <h3>Tools</h3>
+            <ul>
+                <li>ReactJS</li>
+                <li>JavaScript</li>
+                <li>HTML5</li>
+                <li>CSS3</li>
+                <li>NodeJS</li>
+                <li>Express</li>
+                <li>GraphiQL</li>
+                <li>Rest API</li>
+                <li>MongoDB</li>
+                <li>Git</li>
+                <li>Postman</li>
+                <li>Jenkings</li>
+                <li>SQL</li>
+            </ul>
+            <h3>Work methodology</h3>
+            <ul>
+                <li>Agile</li>
+                <li>Canban</li>
+                <li></li>
+                <li></li>
+            </ul>
         </div>
     )
 }
