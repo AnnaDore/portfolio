1f8a936144e73c1b31c7ffeef5458ff77515156f
anna dodo <anna.dorenskaya@gmail.com>
2021-03-09 23:23:38 +0100

    header base

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import "./header.scss"
 
 export default function Header() {
-    return (
-        <div>
-            Header
-        </div>
-    )
+  return (
+    <header>
+      <div class="logo">Ann Do</div>
+      <div class="container">
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+      <div class="nav">
+        <ul>
+          <a>Skills</a>
+          <a>Experience</a>
+          <a>Projects</a>
+          <a>Contact</a>
+        </ul>
+      </div>
+    </header>
+  );
 }
