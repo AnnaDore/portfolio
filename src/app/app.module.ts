ea8407419b4801d2fb34589d7fe6a249bb2ad9f2
annadore <anna.dorenskaya@gmail.com>
2025-01-04 22:30:13 +0100

    add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,20 @@ import { MatIconRegistry, MatIconModule } from '@angular/material/icon';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { MainComponent } from './main/main.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { MainComponent } from './components/main/main.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
+import { SertificatesComponent } from './components/sertificates/sertificates.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    MainComponent
+    MainComponent,
+    AboutMeComponent,
+    SertificatesComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/sertificates/sertificates.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sertificates',
+  standalone: false,
+  
+  templateUrl: './sertificates.component.html',
+  styleUrl: './sertificates.component.scss'
+})
+export class SertificatesComponent {
+
+}
